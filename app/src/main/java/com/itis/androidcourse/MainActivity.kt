037e3a9ed88e4141f085726f78c829c15e930569
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-10-02 12:24:21 +0300

    added hw1

--- app/src/main/java/com/itis/androidcourse/MainActivity.kt
+++ app/src/main/java/com/itis/androidcourse/MainActivity.kt
@@ -2,10 +2,27 @@ package com.itis.androidcourse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.itis.androidcourse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            btnEdit.setOnClickListener {
+                if (tvName.visibility == View.GONE) {
+                    tvName.visibility = View.VISIBLE
+                    ivArrow.visibility = View.VISIBLE
+                } else {
+                    tvName.visibility = View.GONE
+                    ivArrow.visibility = View.GONE
+                }
+            }
+        }
     }
 }
